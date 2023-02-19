3567407e2fe0db78c9fee55c7dd677079aeb2298
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2023-02-18 20:32:20 -0800

    MVP
    OpenAI API Key is publicly available in this commit. This key will be destroyed and a new one issued once the project is nearing completion.

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-async function fetchData() {
-  const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-  const record=await res.json();
-  document.getElementById("date").innerHTML=record.data[0].date;
-  document.getElementById("areaName").innerHTML=record.data[0].areaName;
-  document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-  document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();                         
+const urlElement = document.getElementById('url');
+const responseElement = document.getElementById('response');
+const printElement = document.getElementById('print');
+
+chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+  const domain = new URL(tabs[0].url).hostname;
+  urlElement.innerText = `What do you think of ${domain}?`;
+  const apiKey = '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';
+  const prompt = `What do you think of ${domain}?`;
+  const temperature = 0.7;
+  const maxTokens = 150;
+
+  const body = {
+    prompt,
+    temperature,
+    max_tokens: maxTokens,
+  };
+
+  const response = await fetch("https://api.openai.com/v1/engines/davinci/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+  const answer = data.choices && data.choices.length > 0 ? data.choices[0].text.trim() : null;
+
+  if (answer) {
+    responseElement.innerText = answer;
+  } else {
+    responseElement.innerText = "Error: No answer received from API.";
+  }
+
+  console.log("Domain:", domain);
+  console.log("API Key:", apiKey);
+  console.log("Prompt:", prompt);
+  console.log("Temperature:", temperature);
+  console.log("Max Tokens:", maxTokens);
+  console.log("Response:", data);
+  console.log("Answer:", answer);
+
+  printElement.innerHTML = `
+    <p>Domain: ${domain}</p>
+    <p>API Key: ${apiKey}</p>
+    <p>Prompt: ${prompt}</p>
+    <p>Temperature: ${temperature}</p>
+    <p>Max Tokens: ${maxTokens}</p>
+    <p>Response: ${JSON.stringify(data)}</p>
+    <p>Answer: ${answer}</p>
+  `;
+});
