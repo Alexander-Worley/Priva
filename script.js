6d5abad0985296083e0461cde706bc8a120e6dcd
Grace <gkzhou@stanford.edu>
2023-02-19 05:18:21 -0800

    UI changes and added second prompt

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const urlElement = document.getElementById('url');
 const responseElement = document.getElementById('response');
 const printElement = document.getElementById('print');
 const companyElement = document.getElementById('company');
+const collectionElement = document.getElementById('collection');
 var pageContent;
 
 chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
@@ -11,12 +12,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
 });
   const domain = new URL(tabs[0].url).hostname.replace(/.+\/\/|www.|\..+/g, '');
   companyElement.innerText = domain.charAt(0).toUpperCase() + domain.slice(1);;
-  urlElement.innerText = `What do you think of ${domain}?`;
-  const apiKey = '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';
+  // urlElement.innerText = `What do you think of ${domain}?`;
+  const apiKey = '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';
   console.log("page content", pageContent)
-  const prompt = `Summarize the privacy policy of ${domain}`;
+  const prompt = `State the sensitive personal data ${domain} collects in 200 words or less.`;
   const temperature = 0.5;
-  const maxTokens = 150; 
+  const maxTokens = 200; 
 
   // urlElement.innerText = `give me a bullet point list of the sensitive personal data is collected according to ${domain}?`;
   // const apiKey = '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';
@@ -46,6 +47,34 @@ chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
   } else {
     responseElement.innerText = "Error: No answer received from API.";
   }
+  
+  // Data collected
+  const prompt2 = `Summarize what sensitive personal data ${domain} collects`;
+  const body2 = {
+    prompt : prompt2,
+    temperature,
+    max_tokens: maxTokens,
+  };
+
+  const response2 = await fetch("https://api.openai.com/v1/engines/davinci/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify(body2)
+  });
+  const data2 = await response2.json();
+  const answer2 = data2.choices && data2.choices.length > 0 ? data2.choices[0].text.trim() : null;
+  
+  console.log("data", data2)
+  console.log("answer", answer2)
+  if (answer2) {
+    collectionElement.innerText = answer2;
+  } else {
+    collectionElement.innerText = "Error: No answer received from API.";
+  }
+
   console.log("Domain:", domain);
   console.log("API Key:", apiKey);
   console.log("Prompt:", prompt);
